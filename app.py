a7ab79b3021a5ce2a8ce0e90c4db04fc610ed72a
anika Vats <anikavats@anikas-MacBook-Air.local>
2026-01-01 11:04:16 -0800

    This is dictionaries

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-name = 'Arianna'
-score = 75
-if score >= 90:
-    print("Grade: A")
-elif score >= 80:
-    print("Grade: B")
-elif score >= 70:
-    print("Grade: C")
-elif score >= 60:
-    print("Grade: D")
-else:
-    print("Grade: F")
-    
+my_dict = {'key1':'value1','key2':'value2'}
+my_dict
+my_dict['key1']
+prices_lookup = {'apples':2.99,'oranges':3.99,'milk':1.99}
+prices_lookup = ['apples']
