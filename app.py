023d84929744c6e96ef04f0877c7f2f870278608
anika Vats <anikavats@anikas-MacBook-Air.local>
2026-01-02 11:19:02 -0800

    basic files

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-age = 25
-has_license = True
-can_drive = age >= 18 and has_license
-print(f"Can drive: {can_drive}")
 
-is_weekend = True
-is_holiday = False
-can_rest = is_weekend or is_holiday
-print(f"Can rest: {can_rest}")
-
-is_raining = False
-go_outside = not is_raining
-print(f"Go outside: {go_outside}")
-False
-True
-3.9999999999999999999999>3.99999999999999999999999999
-
+with open("note.txt", "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content)
+# Not a lot but still
+with open("happy.txt","w") as f:
+    content = f.write('help')
+    print('help')
